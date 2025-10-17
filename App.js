08ceae1cf9fe7015c30cf36cf7446bf0204c7cdf
Kiran kumar konanki <131464273+kirankonanki@users.noreply.github.com>
2025-10-17 12:34:59 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import EmployeeList from "./components/EmployeeList";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Employee Management System</h1>
+      <EmployeeList />
+    </div>
+  );
+}
+
+export default App;
